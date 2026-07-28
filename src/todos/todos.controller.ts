731dec549dede552af456a8194a95865c68fc1d1
during94 <during105@naver.com>
2026-07-28 15:14:49 +0900

    feat: Todo 생성 요청 DTO 검증 추가

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -1,4 +1,15 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CreateTodoDto } from './dto/create-todo.dto';
 
 @Controller('todos')
 export class TodosController {
@@ -15,5 +26,31 @@ export class TodosController {
   @Get(':id')
   findOne(@Param('id') id: string): string {
     return `${id}번 Todo`;
+  }
+
+  @Post()
+  create(@Body() body: CreateTodoDto) {
+    return {
+      id: 1,
+      title: body.title,
+      completed: false,
+    };
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() body: { title?: string; completed?: boolean },
+  ) {
+    return {
+      id,
+      ...body,
+    };
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  remove(@Param('id') id: string): void {
+    console.log(`${id}번 todo 삭제`);
   }
 }
